96c5dd60d8e130f9879e2316597ab379b1d9f693
ClaraSimonsson <clara.simonsson@hotmail.se>
2022-04-08 13:14:10 +0200

    Flaks tutorial

--- flask/flaskr/__init__.py
+++ flask/flaskr/__init__.py
@@ -2,6 +2,8 @@ import os
 
 from flask import Flask
 from . import db
+from . import auth
+from . import blog
 
 # from routes import routes_blueprint
 
@@ -9,7 +11,10 @@ from . import db
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    #db.init_app(app)
+    db.init_app(app)
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(blog.bp)
+    app.add_url_rule('/', endpoint='index')
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
--- flask/flaskr/db.py
+++ flask/flaskr/db.py
@@ -5,23 +5,22 @@ import click
 from flask import current_app, g
 from flask.cli import with_appcontext
 
+def init_db():
+    db = get_db()
+
+    with current_app.open_resource('../db/schema.sql') as f:
+        db.executescript(f.read().decode('utf8'))
+
 @click.command('init-db')
 @with_appcontext
 def init_db_command():
     """Clear the existing data and create new tables."""
     init_db()
     click.echo('Initialized the database.')
-
-def init_db():
-    db = get_db()
-
-    with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
-
-
+    
 def init_app(app):
     app.teardown_appcontext(close_db)
-    get_db()
+    app.cli.add_command(init_db_command)
 
 def get_db():
     if 'db' not in g:
@@ -38,6 +37,7 @@ def close_db(e=None):
 
     if db is not None:
         db.close()
+
 
 
 '''def get_db():
