64236a6046743552129e68e19c8392cc8944a1cb
emillindblad <emil@lindblad.tech>
2022-04-27 10:34:57 +0200

    db can now be inited

--- flask/db/schema.sql
+++ flask/db/schema.sql
@@ -25,11 +25,11 @@ CREATE TABLE Patients (
 
 CREATE TABLE Cases (
     caseId UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    patient CHAR(13) NOT NULL,
+    patient CHAR(4) NOT NULL,
     reviewedBy TEXT NOT NULL,
     reviewDate TIMESTAMP NOT NULL,
     closed BOOL NOT NULL,
-    FOREIGN KEY (patient) REFERENCES Patients (idnr)
+    FOREIGN KEY (patient) REFERENCES Patients (key_id)
 );
 
 CREATE TABLE Treatments (
@@ -41,11 +41,11 @@ CREATE TABLE Treatments (
     PRIMARY KEY (treatmentId,caseId)
 );
 
-INSERT INTO Patients (idnr,name,age,weight,bmi,nicotine,adress,city,zipcode) VALUES ('20000901-1234', 'Foo Bar', '69', '420', '21.2', 'Nej', 'Hubbenvägen 1','Göteborg','41280');
-INSERT INTO Patients (idnr,name,age,weight,bmi,nicotine,adress,city,zipcode) VALUES ('19940418-6234', 'Por Tals', '35', '098', '21.59', 'Ja, lmao', 'Kemivägen 1','Göteborg','43331');
+INSERT INTO Patients (key_id,idnr,name,age,weight,bmi,nicotine,adress,city,zipcode) VALUES ('0001','20000901-1234', 'Foo Bar', '69', '420', '21.2', 'Nej', 'Hubbenvägen 1','Göteborg','41280');
+INSERT INTO Patients (key_id,idnr,name,age,weight,bmi,nicotine,adress,city,zipcode) VALUES ('0002','19940418-6234', 'Por Tals', '35', '098', '21.59', 'Ja, lmao', 'Kemivägen 1','Göteborg','43331');
 
-INSERT INTO Cases (patient,reviewedBy,reviewDate,closed) VALUES ('20000901-1234','Kingen','2022-04-19','TRUE');
-INSERT INTO Cases (caseId,patient,reviewedBy,reviewDate,closed) VALUES ('a789ea53-08b6-4108-a1d5-e992c2413654','19940418-6234','skrr','2022-09-23 12:30','FALSE');
+INSERT INTO Cases (patient,reviewedBy,reviewDate,closed) VALUES ('0001','Kingen','2022-04-19','TRUE');
+INSERT INTO Cases (caseId,patient,reviewedBy,reviewDate,closed) VALUES ('a789ea53-08b6-4108-a1d5-e992c2413654','0002','skrr','2022-09-23 12:30','FALSE');
 
 INSERT INTO Treatments (caseId,parameter1,parameter2) VALUES ('a789ea53-08b6-4108-a1d5-e992c2413654','TESTING','HELLO WORLD!');
 
