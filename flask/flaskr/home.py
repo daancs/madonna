c843cc731dd266c6cff2969a7c8a4e45842b35a6
emillindblad <emil@lindblad.tech>
2022-05-03 10:59:25 +0200

    created files for homepage

---
+++ flask/flaskr/home.py
@@ -0,0 +1,17 @@
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.exceptions import abort
+
+from flaskr.auth import login_required
+from flaskr.db import get_db
+
+bp = Blueprint('home', __name__)
+
+@bp.route('/home')
+@login_required
+def home():
+    if g.user is None:
+        return redirect(url_for('auth.login'))
+    else:
+        return render_template('home/home.html')
