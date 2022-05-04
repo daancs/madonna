683a406c456412347a34c5f7e8f66c4623135e28
emillindblad <emil@lindblad.tech>
2022-05-04 13:51:55 +0200

    search history on home page

--- flask/flaskr/home.py
+++ flask/flaskr/home.py
@@ -2,6 +2,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.exceptions import abort
+import psycopg2, psycopg2.extras
 
 from flaskr.auth import login_required
 from flaskr.db import get_db
@@ -11,7 +12,12 @@ bp = Blueprint('home', __name__)
 @bp.route('/home')
 @login_required
 def home():
+    conn = get_db()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT * FROM SearchHistory")
+    history = cur.fetchall()
+    print(history)
     # if g.user is None:
         # return redirect(url_for('auth.login'))
     # else:
-        return render_template('home/home.html')
+    return render_template('home/home.html', history=history)
