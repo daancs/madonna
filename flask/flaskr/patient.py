5c5d77aa6b9d27ae68be947b3c6f6eb38c58cff9
ClaraSimonsson <clara.simonsson@hotmail.se>
2022-05-09 15:49:05 +0200

    patient view more structured

--- flask/flaskr/patient.py
+++ flask/flaskr/patient.py
@@ -16,9 +16,21 @@ def patient(key_id):
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     query = "SELECT * FROM Patients LEFT JOIN Cases ON patients.key_id=cases.patient WHERE Patients.key_id =(%s)"
     cur.execute(query, (key_id,))
+
+    ''' Används för att få ut kolumnnamnen på det som joinats, används inte i detta fall eftersom
+    de är på engelska
+    column_names = [desc[0] for desc in cur.description]
+    for i in column_names:
+        print(i)
+    '''
+    
+    patient_col_names = ['Key-ID', 'Personnummer', 'Namn', 'Ålder', 'Kön', 'Vikt', 'BMI', 'Nikotin', 'Avliden', 'Adress', 'Stad', 
+    'Postnummer']
+    cases_col_names = ['Case-ID', 'Patient', 'Komplikation', 'Granskad av', 'Granskningsdatum', 'Studie avslutad']
     conn.commit()
     result = cur.fetchall()
-    return render_template('patient/patientview.html', result=result[0])
+
+    return render_template('patient/patientview.html', result=result[0], patient_col_names=patient_col_names, cases_col_names=cases_col_names)
 
 
 """ def buildPatientQuery(key_id):
