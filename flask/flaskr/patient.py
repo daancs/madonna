ec28903366092615bec2c98cdf07b9aaf83a484a
emillindblad <emil@lindblad.tech>
2022-05-11 11:29:51 +0200

    Started working on edit form

--- flask/flaskr/patient.py
+++ flask/flaskr/patient.py
@@ -34,7 +34,25 @@ def patient(key_id):
 
 @bp.route('/patient/<key_id>/edit', methods=['GET', 'POST'])
 def editPatient(key_id):
-    return "dab"
+    conn = db.get_db()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    query = "SELECT * FROM Patients LEFT JOIN Cases ON patients.key_id=cases.patient WHERE Patients.key_id =(%s)"
+    cur.execute(query, (key_id,))
+
+    ''' Används för att få ut kolumnnamnen på det som joinats, används inte i detta fall eftersom
+    de är på engelska
+    column_names = [desc[0] for desc in cur.description]
+    for i in column_names:
+        print(i)
+    '''
+
+    patient_col_names = ['Key-ID', 'Personnummer', 'Namn', 'Ålder', 'Kön', 'Vikt', 'BMI', 'Nikotin', 'Avliden', 'Adress', 'Stad',
+    'Postnummer']
+    cases_col_names = ['Case-ID', 'Patient', 'Komplikation', 'Granskad av', 'Granskningsdatum', 'Studie avslutad']
+    conn.commit()
+    result = cur.fetchall()
+
+    return render_template('patient/editPatient.html', result=result[0], patient_col_names=patient_col_names, cases_col_names=cases_col_names)
 
 """ def buildPatientQuery(key_id):
     query = SELECT * FROM Patients WHERE Patients.key_id = %s INNER JOIN Cases ON Patients.key_id=Cases.patient, (key_id,)
