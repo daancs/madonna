eeb9d96205ff28024c170ea50460fc661fd3ae5e
emillindblad <emil@lindblad.tech>
2022-05-13 11:43:19 +0200

    Edit patient data now possible, fix needed for redirect after change

--- flask/flaskr/patient.py
+++ flask/flaskr/patient.py
@@ -9,66 +9,61 @@ from . import db
 
 bp = Blueprint('patient', __name__)
 
+patientColNames = ['Key-ID', 'Personnummer', 'Namn', 'Ålder', 'Kön', 'Vikt', 'BMI', 'Nikotin', 'Avliden', 'Adress', 'Stad','Postnummer']
+casesColNames = ['Case-ID', 'Patient', 'Komplikation', 'Granskad av', 'Granskningsdatum', 'Studie avslutad']
 
-@bp.route('/patient/<key_id>')
+@bp.route('/patient/<key_id>', methods=['GET'])
 def patient(key_id):
-    conn = db.get_db()
-    cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
-    query = "SELECT * FROM Patients LEFT JOIN Cases ON patients.key_id=cases.patient WHERE Patients.key_id =(%s)"
-    cur.execute(query, (key_id,))
 
-    ''' Används för att få ut kolumnnamnen på det som joinats, används inte i detta fall eftersom
-    de är på engelska
-    column_names = [desc[0] for desc in cur.description]
-    for i in column_names:
-        print(i)
-    '''
+    result = getPatientData(key_id)
 
-    patient_col_names = ['Key-ID', 'Personnummer', 'Namn', 'Ålder', 'Kön', 'Vikt', 'BMI', 'Nikotin', 'Avliden', 'Adress', 'Stad',
-    'Postnummer']
-    cases_col_names = ['Case-ID', 'Patient', 'Komplikation', 'Granskad av', 'Granskningsdatum', 'Studie avslutad']
-    conn.commit()
-    result = cur.fetchall()
-
-    return render_template('patient/patientview.html', result=result[0], patient_col_names=patient_col_names, cases_col_names=cases_col_names)
+    return render_template('patient/patientview.html', result=result[0], patientColNames=patientColNames, casesColNames=casesColNames)
 
 @bp.route('/patient/<key_id>/edit', methods=['GET', 'POST'])
 def editPatient(key_id):
+    if request.method == 'POST':
+        print(key_id)
+        conn = db.get_db()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        query = """ UPDATE Patients SET
+                    idnr = %s,
+                    name = %s,
+                    age = %s,
+                    gender = %s,
+                    weight = %s,
+                    bmi = %s,
+                    nicotine = %s,
+                    deceased = %s,
+                    adress = %s,
+                    city = %s,
+                    zipcode = %s
+                    WHERE key_id = %s
+                """
+
+        cur.execute(query, (request.form['idnr'], request.form['name'],
+                            request.form['age'], request.form['gender'],
+                            request.form['weight'], request.form['bmi'],
+                            request.form['nicotine'], request.form['deceased'],
+                            request.form['adress'], request.form['city'],
+                            request.form['zipcode'], request.form['key_id'])
+                    )
+
+        conn.commit()
+        return "dab"
+
+    result = getPatientData(key_id)
+    return render_template('patient/editPatient.html', result=result[0], formColNames=formColNames, patientColNames=patientColNames, casesColNames=casesColNames)
+
+def exit():
+    return redirect(url_for('guiQuery.index'))
+
+def getPatientData(key_id):
     conn = db.get_db()
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     query = "SELECT * FROM Patients LEFT JOIN Cases ON patients.key_id=cases.patient WHERE Patients.key_id =(%s)"
     cur.execute(query, (key_id,))
-
-    ''' Används för att få ut kolumnnamnen på det som joinats, används inte i detta fall eftersom
-    de är på engelska
-    column_names = [desc[0] for desc in cur.description]
-    for i in column_names:
-        print(i)
-    '''
-
-    patient_col_names = ['Key-ID', 'Personnummer', 'Namn', 'Ålder', 'Kön', 'Vikt', 'BMI', 'Nikotin', 'Avliden', 'Adress', 'Stad',
-    'Postnummer']
-    cases_col_names = ['Case-ID', 'Patient', 'Komplikation', 'Granskad av', 'Granskningsdatum', 'Studie avslutad']
     conn.commit()
-    result = cur.fetchall()
+    global formColNames # Sets formColNames as a global variable for use in editPatient()
+    formColNames = [desc[0] for desc in cur.description]
 
-    return render_template('patient/editPatient.html', result=result[0], patient_col_names=patient_col_names, cases_col_names=cases_col_names)
-
-""" def buildPatientQuery(key_id):
-    query = SELECT * FROM Patients WHERE Patients.key_id = %s INNER JOIN Cases ON Patients.key_id=Cases.patient, (key_id,)
-    return query
-
-def runQuery(query):
-
-    This function runs the query in postgresql.
-
-    database = db.get_db()
-    cur = database.cursor(cursor_factory=psycopg2.extras.DictCursor) # needed for select queries
-    cur.execute(query)
-    database.commit()
-    res = cur.fetchall()
-    return res
-"""
-
-def exit():
-    return redirect(url_for('guiQuery.index'))
+    return cur.fetchall()
