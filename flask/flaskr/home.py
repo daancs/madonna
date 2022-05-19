c2da4e66e19196494aaef0c569fd8b6626699560
emillindblad <emil@lindblad.tech>
2022-05-20 00:00:43 +0200

    Reformated changeLog, now userfriendly (ish)

--- flask/flaskr/home.py
+++ flask/flaskr/home.py
@@ -20,24 +20,35 @@ def home():
     cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
     cur.execute("SELECT * FROM SearchHistory")
     searchHistory = cur.fetchall()
-
-    cur.execute("SELECT tstamp, operation, who, new_val, old_val FROM changeLog")
+    cur.execute("SELECT tstamp, who, old_val, new_val FROM changeLog")
     changeLog = []
     for entry in cur.fetchall():
         changeLog.append(changeLogParser(entry))
 
-    print(changeLog)
-    # if g.user is None:
-        # return redirect(url_for('auth.login'))
-    # else:
+    # print(changeLog[0][2]['values_changed'].items())
+
+    # for foo in changeLog[0][2]['values_changed'].items():
+        # print(f"values_changed: {foo[0]}")
+        # for bar in foo[1]:
+            # print(f"{bar}: {foo[1][bar]}")
+
+
+
     return render_template('home/home.html', searchHistory=searchHistory, changeLog=changeLog)
 
 
 def changeLogParser(entry):
-    out = entry
-    out[0] = entry[0].strftime("%Y-%m-%d %H:%M:%S")
-    print(entry[3]['key_id'])
-    diff = DeepDiff(entry[3], entry[4])
-    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
-    print(json.dumps(diff, indent=4))
+    out = []
+
+    #Timestamp
+    out.append(entry[0].strftime("%Y-%m-%d %H:%M:%S"))
+
+    #User who made the change
+    out.append(entry[1])
+
+    # entry[3] == new_val, entry[4] == old_val
+    diff = DeepDiff(entry[2], entry[3])
+    # print(json.dumps(diff, indent=4))
+    diff=diff.to_dict()
+    out.append(diff)
     return out
